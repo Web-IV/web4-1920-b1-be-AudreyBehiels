a3b5eeffe7e362b25ff68fec7573e78a5aa277f5
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-03-26 20:55:49 +0100

    domein klasses, repo en controller toegevoegd

---
+++ WebappsIV_1920_be_AudreyB/Models/IFilmRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebappsIV_1920_be_AudreyB.Models
+{
+    public interface IFilmRepository
+    {
+        IEnumerable<Film> GetAllFilms();
+        IEnumerable<Film> GetFilmsByTitel(string titel);
+        IEnumerable<Film> GetFilmsByYear(DateTime jaar);
+        IEnumerable<Film> GetFilmsByGenre(string genre);
+
+        void SaveChanges();
+    }
+}
