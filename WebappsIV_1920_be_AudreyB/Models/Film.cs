75fe9bc23a3aba80150791e30c33943400de738b
Audrey Behiels <audrey.behiels@student.hogent.be>
2020-03-29 22:08:08 +0200

    De GET van GetAllFilms de using van JsonIgnore aangepast

--- WebappsIV_1920_be_AudreyB/Models/Film.cs
+++ WebappsIV_1920_be_AudreyB/Models/Film.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace WebappsIV_1920_be_AudreyB.Models
